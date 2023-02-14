c062bc74f48a355b90350b92872e95e41ed8fc46
liuhongbo <liuhongbo@dip-ai.com>
2023-02-14 16:14:09 +0800

    feat: pull code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,20 @@
 /*
  * @Author: liuhongbo liuhongbo@dip-ai.com
  * @Date: 2023-02-10 17:26:55
- * @LastEditors: liuhongbo 916196375@qq.com
- * @LastEditTime: 2023-02-12 17:12:09
+ * @LastEditors: liuhongbo liuhongbo@dip-ai.com
+ * @LastEditTime: 2023-02-10 17:44:35
  * @FilePath: /minibbs/src/app.module.ts
  * @Description: 主程序入口
  */
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { dataBaseConfig } from './const'
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { FriendModule } from './friend/friend.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataBaseConfig),
-    UserModule,
-    AuthModule,
-    FriendModule,
+    
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
