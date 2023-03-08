88e8ecfe0b0768b94b01bdbbb0dba8bec186c83b
liuhongbo <916196375@qq.com>
2023-03-08 23:58:40 +0800

    feat: change app server

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@
  * @LastEditors: liuhongbo 916196375@qq.com
  * @LastEditTime: 2023-03-08 21:59:01
  * @FilePath: \MINIBBS_NEST\src\app.service.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: hellow wrold
  */
 import { Injectable } from '@nestjs/common';
 
